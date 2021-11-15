8ed35e0b8854b35c62af4a6ed1cb06489de8ee09
Danya Shlyahov <ukraniandollar@gmail.com>
2021-11-15 16:48:21 +0200

    Finish

--- main.cpp
+++ main.cpp
@@ -3,32 +3,63 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
+#include "EBO.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "Libraries/include/glm/glm.hpp"
+#include "Libraries/include/glm/gtc/matrix_transform.hpp"
+#include "Libraries/include/glm/gtc/type_ptr.hpp"
+#include "shaderClass.h"
 
 
 int main() {
+	//HWND console_window = GetConsoleWindow();
+	//ShowWindow(console_window, false);
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3))* 2 / 3, 0.0f
+	GLfloat vertices[] =
+	{
+		-0.6f, 0.6f, 0.0f, // 0
+		-0.6f, 0.4f, 0.0f, // 1
+		-0.4f, 0.4f, 0.0f, // 2
+		-0.4f, 0.6f, 0.0f, // 3
+		-0.2f, 0.7f, 0.0f,  // 4
+		-0.2f, 0.6f, 0.0f, // 5
+		-0.4f, 0.5f, 0.0f, // 6
+		-0.4f, 0.0f, 0.0f, // 7
+		0.0f, 0.0f, 0.0f, // 8
+		-0.2f, 0.25f, 0.0f, // 9
+		0.3f, 0.25f, 0.0f, // 10
+		0.3f, -0.4f, 0.0f, // 11
+		0.3f, 0.45f, 0.0f, // 12
+		0.5f, 0.25f, 0.0f, // 13
+		0.5f, 0.45f, 0.0f, // 14
+		0.3f, 0.0f, 0.0f, // 15
+		0.6f, -0.4f, 0.0f, //16
+		0.3f, -0.8f, 0.0f, // 17
+		-0.6f, 0.0f, 0.0f, // 18
+		-0.6f, -0.2f, 0.0f, //19
+		-0.25f, -0.2f, 0.0f, //20
+		-0.25f, 0.0f, 0.0f, // 21
+		-0.8f, -0.2f, 0.0f, //22
+		-0.1f, 0.0f, 0.0f //23
+	};
+
+	GLuint indices[] = {
+		0, 1, 2,
+		0, 3, 2,
+		3, 4, 5,
+		6, 7, 8,
+		9, 10, 11,
+		10, 13, 14,
+		15, 16, 17,
+		18, 19, 20,
+		18, 20, 21,
+		18, 19, 22,
+		20, 21, 23
 	};
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "I made this!", NULL, NULL);
@@ -45,61 +76,68 @@ int main() {
 
 	glViewport(0, 0, 800, 800);
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	Shader shader("default.vert", "default.frag");
 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	VAO VAO1;
+	VAO1.Bind();
 
-	GLuint shaderProgram = glCreateProgram();
-	
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
 
-	glLinkProgram(shaderProgram);
+	VAO1.LinkVBO(VBO1, 0);
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	GLuint transformLoc = glGetUniformLocation(shader.ID, "transform");
 
-	GLuint VAO, VBO;
-	
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	glm::mat4 transform = glm::mat4(1.0f);
 
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-
-
-
-	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	//glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+	//glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
+	GLuint lenIndeces = sizeof(indices) / sizeof(GLfloat);
+
+	VAO1.Bind();
 	while (!glfwWindowShouldClose(window)) {
-		glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+		glClearColor(0.8f, 0.1f, 0.4f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-			
+		shader.Activate();
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+		glDrawElements(GL_TRIANGLES, lenIndeces, GL_UNSIGNED_INT, 0);
+		if (glfwGetKey(window, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS) {
+			transform = glm::scale(transform, glm::vec3(0.99, 0.99, 0.0));
+		}
+		if (glfwGetKey(window, GLFW_KEY_KP_ADD) == GLFW_PRESS) {
+			transform = glm::scale(transform, glm::vec3(1.01, 1.01, 0.0));
+		}
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			transform = glm::translate(transform, glm::vec3(0.0f, 0.005f, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			transform = glm::translate(transform, glm::vec3(0.0f, -0.005f, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			transform = glm::translate(transform, glm::vec3(0.005f, 0.0f, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			transform = glm::translate(transform, glm::vec3(-0.005f, 0.0f, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+			transform = glm::rotate(transform, glm::radians(3.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+			transform = glm::rotate(transform, glm::radians(-3.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shader.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
